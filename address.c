b12638a4a84c1c70753c10fcbdb43bac13e85849
Manvi9312 <manvigupta0308@gmail.com>
2025-09-04 22:07:10 +0530

    Add files via upload

---
+++ address.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("Manvi Gupta\n");
+    printf("123, Green Street\n");
+    printf("Jaipur, Rajasthan\n");
+    printf("India\n");
+    printf("PIN: 302001\n");
+    return 0;
+}
