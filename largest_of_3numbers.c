7d316b23629746683053c2961989320003f9d46b
MANVI GUPTA <manvigupta0308@gmail.com>
2025-09-09 11:58:41 +0530

    Add files via upload

---
+++ largest_of_3numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main(){
+    int a,b,c,largest;
+
+    printf("enter first number: ");
+    scanf("%d",&a);
+
+    printf("enter second number: ");
+    scanf("%d",&b);
+
+    printf("enter third number: ");
+    scanf("%d",&c);
+
+    largest=a>b?((a>c)?a:c):((b>c)?b:c);
+
+    printf("largest number is: %d",largest);
+}
