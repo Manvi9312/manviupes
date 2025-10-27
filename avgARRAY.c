dba5f62aced40f7e1992244397a9865491c5b0cb
MANVI GUPTA <manvigupta0308@gmail.com>
2025-10-27 14:00:03 +0530

    Add files via upload

---
+++ avgARRAY.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+// Function to calculate average
+float findAverage(int arr[], int n) {
+    int sum = 0;
+    for(int i = 0; i < n; i++) {
+        sum += arr[i];
+    }
+    return (float)sum / n;
+}
+
+int main() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d numbers:\n", n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    float avg = findAverage(arr, n);
+    printf("Average = %.2f\n", avg);
+
+    return 0;
+}
